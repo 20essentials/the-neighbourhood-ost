a76daeaf9355fa178c362f6826f46f847310249c
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 23:17:01 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THE NEIGHBOURHOOD';
+export let nameSongs = [
+	"SOFTCORE", /*1*/
+	"DADDY ISSUES", /*2*/
+	"CRY BABY", /*3*/
+	"AFRAID", /*4*/
+	"THE BEACH", /*5*/
+	"NERVOUS", /*6*/
+	"SWEATER WEATHER", /*7*/
+	"SINGLE", /*8*/
+	"ALLEYWAYS", /*9*/
+	"HOW", /*10*/
+	"REFLECTIONS", /*11*/
+	"HEAVEN", /*12*/
+	"PREY", /*13*/
+	"FLOAT", /*14*/
+	"STAYING UP", /*15*/
+	"FLAWLESS", /*16*/
+	"NOISE", /*17*/
+	"24/7", /*18*/
+	"CRYBABY - EDIT", /*19*/
+	"WEST COAST", /*20*/
+]
